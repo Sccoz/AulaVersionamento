a82e1fef24c719c2ff46919fd9eb7c792a015901
Yago Scoz <yagoscoz10@gmail.com>
2026-05-20 19:20:46 -0300

    Adicao Potencia e %

--- calculadora.py
+++ calculadora.py
@@ -5,10 +5,10 @@ print("================================")
 
 while True:
     print("Você vai ter algumas escolhas a seguir:") 
-    print("1 - Soma\n2 - Subtração\n3 - Divisão\n4 - Multiplicação\n0 - Sair")
+    print("1 - Soma\n2 - Subtração\n3 - Divisão\n4 - Multiplicação\n5 - Exponenciacao\n6 - Calc Porcent \n0 - Sair")
     escolha = int(input("\nDigite a opção: "))
 
-    if escolha != 0:
+    if escolha == 0:
         print("Saindo... Até logo!")
         break
 
@@ -27,6 +27,10 @@ while True:
                 print("Impossivel o denomiandor ser 0 !!!")
         case 4:
             resultado = n1 * n2
+        case 5 :
+            resultado = n1 ** n2
+        case 6 :
+            resultado = n1 * (n2 /100)
         case _:  
             resultado = "Operação inválida"
 
